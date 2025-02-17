78b28e1e6f25043e2cdf3de146db7ef0123162f9
theAbdoSabbagh <abdulghani.alaa.sabbagh@gmail.com>
2025-02-17 13:39:34 +0400

    rewritten, organized better

--- DeeperSeek/internal/selectors.py
+++ DeeperSeek/internal/selectors.py
@@ -1,41 +1,40 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
-class DeepSeekSelectors:    
-    # Login
-    email_input_css = 'input[type="text"]'
-    password_input_css = 'input[type="password"]'
-    confirm_checkbox_css = 'div[class="ds-checkbox ds-checkbox--none ds-checkbox--bordered"]'
-    login_button_css = 'div[role="button"]'
+class LoginSelectors:
+    email_input: str = 'input[type="text"]'
+    password_input: str = 'input[type="password"]'
+    confirm_checkbox: str = 'div[class="ds-checkbox ds-checkbox--none ds-checkbox--bordered"]'
+    login_button: str = 'div[role="button"]'
 
-    # Interactions
-    textbox_css = 'textarea[class="c92459f0"]'
-    send_options_parent_css = 'div[class="ec4f5d61"]'
-    send_button_css = 'div[class="f6d670"]'
-    deepthink_button_css = 'div[class="d9f56c96"]'
-    search_button_css = 'div[class="ad0c98fd"]'
-    response_toolbar_css = 'div[class="ds-flex abe97156"]'
-    """First div: **copy** `||` Second div: **regenerate** `||` Third div: **like** `||` Fourth div: **dislike**"""
-    reset_chat_button_css = 'div[class="e214291b"]'
+@dataclass
+class InteractionSelectors:
+    textbox: str = 'textarea[class="c92459f0"]'
+    send_options_parent: str = 'div[class="ec4f5d61"]'
+    send_button: str = 'div[class="f6d670"]'
+    response_toolbar: str = 'div[class="ds-flex abe97156"]'
+    reset_chat_button: str = 'div[class="e214291b"]'
+    search_results: str = 'div[class="fe369d61 f529c936"]'
+    deepthink_content: str = 'div[class="e1675d8b"]'
+    profile_button: str = 'div[class="ede5bc47"]'
+    profile_options_dropdown: str = 'div[class="ds-floating-position-wrapper ds-theme"]'
+    confirm_deletion_button: str = 'div[class="ds-button ds-button--error ds-button--filled ds-button--rect ds-button--m"]'
+    theme_select_parent: str = 'div[class="ds-native-select ds-native-select--filled ds-native-select--none ds-native-select--m"]'
 
-    # Response data
-    first_extra_option_css = 'div[class="a6d716f5 db5991dd"]'
-    second_extra_option_css = 'div[class="edb250b1"]'
+@dataclass
+class BackendSelectors:
+    response_generating: str = 'div[class="f9bf7997 d7dc56a8"]'
+    response_generated: str = 'div[class="f9bf7997 d7dc56a8 c05b5566"]'
+    regen_loading_icon: str = 'div[class="ds-loading b4e4476b"]'
+    response_toolbar_b64: str = 'ds-flex abe97156'  # This is different, because it's used in BS4
 
-    # Backend
-    conversation_css = 'div[class="dad65929"]'
-    response_generating_css = 'div[class="f9bf7997 d7dc56a8"]'
-    response_generated_css = 'div[class="f9bf7997 d7dc56a8 c05b5566"]'
-    response_css = 'ds-markdown' # This is different, because its used in BS4
-    regen_loading_icon_css = 'div[class="ds-loading b4e4476b"]'
-    response_toolbar_b64_css = 'ds-flex abe97156' # This is different, because its used in BS4
+@dataclass
+class URLSelectors:
+    chat_url: str = "https://chat.deepseek.com/"
 
-    # Search results
-    search_results_div = 'div[class="fe369d61 f529c936"]'
-
-    # DeepThink
-    deepthink_content_css = 'div[class="e1675d8b"]'
-    deepthink_pTags_css = 'ba94db8a'
-    
-    # URLs
-    chat_url = "https://chat.deepseek.com/"
+@dataclass
+class DeepSeekSelectors:
+    login: LoginSelectors = field(default_factory=LoginSelectors)
+    interactions: InteractionSelectors = field(default_factory=InteractionSelectors)
+    backend: BackendSelectors = field(default_factory=BackendSelectors)
+    urls: URLSelectors = field(default_factory=URLSelectors)
